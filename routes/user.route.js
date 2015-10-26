c24b6f02fca03418b39e01a6fbecbe14915e6c10
Ozdemir, Osman (CORP) <Osman.Ozdemir@adp.com>
2015-10-26 00:26:53 -0400

    user route updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,87 +1,163 @@
 'use strict'
-var User = require('../models/user.model');
-var util = require('../util');
-var jwt    = require('jsonwebtoken');
-module.exports = function(app){
+const User = require('../models/user.model');
+const util = require('../util');
+const jwt = require('jsonwebtoken');
+const faker = require('faker');
 
-  app.post('/users/authenticate', function(req, res){
+module.exports = function (app) {
+
+  app.post('/users/authenticate', function (req, res) {
     var auth = req.body;
     auth.password = util.saltAndHashPassword(auth.password);
     User.findOneAsync(auth)
-    .then(function(user){
-      if(user){
-        var token = jwt.sign(user, app.get('secret'), {
-          expiresInMinutes: 15 // expires in 24 hours
-        });
-        delete user.password;
-        res.status(200).json({
-          user: user,
-          token: token
-        });
-      } else {
-        res
-        .status(404)
-        .json({name: "ObjectNotFoundError", message: "The user with given e-amil and password was not found on the system."});
-      }
+        .then(function (user) {
+          if (user) {
+            user = user.toJSON();
+            var token = jwt.sign(user, app.get('secret'), {
+              expiresIn: 900 // expires in 15 mins
+            });
+            delete user.password;
+            res.status(200).json({
+              user: user,
+              token: token
+            });
+          } else {
+            res
+                .status(404)
+                .json({
+                  name: "ObjectNotFoundError",
+                  message: "The user with given e-amil and password was not found on the system."
+                });
+          }
 
-    })
-    .catch(function(err){
-      res.status(500).json({ name: err.name, message: err.message });
-    });
+        })
+        .catch(function (err) {
+          res.status(500).json({
+            name: err.name,
+            message: err.message
+          });
+        });
 
   });
 
-  app.get('/users', authenticate, function(req, res){
+  app.get('/users', authenticate, function (req, res) {
     User.findAsync()
-    .then(function(users){
-      if(users){
-        res.status(200).json(users);
-      } else {
-        res.status(404).json({ name: "ObjectNotFoundError", message: 'No users found been found'});
-      }
+        .then(function (users) {
+          if (users) {
+            res.status(200).json(users);
+          } else {
+            res.status(404).json({
+              name: "ObjectNotFoundError",
+              message: 'No users found been found'
+            });
+          }
 
-    })
-    .catch(function(err){
-      console.log(err.message);
-      res.status(500).json({ name : err.name, message: err.message});
-    });
+        })
+        .catch(function (err) {
+          console.log(err.message);
+          res.status(500).json({
+            name: err.name,
+            message: err.message
+          });
+        });
   });
-  app.get('/users/:id', authenticate, function(req, res){
-    User.findOneAsync({ _id: req.params.id})
-    .then(function(user){
-      if(user){
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ name: "ObjectNotFoundError", message: 'No user found been found with the id ' + req.params.id });
-      }
+
+  app.get('/users/:id', authenticate, function (req, res) {
+    User.findOneAsync({
+      _id: req.params.id
     })
-    .catch(function(err){
-      console.log(err.message);
-      res.status(500).json({ name : err.name, message: err.message})
-    });
+        .then(function (user) {
+          if (user) {
+            res.status(200).json(user);
+          } else {
+            res.status(404).json({
+              name: "ObjectNotFoundError",
+              message: 'No user found been found with the id ' + req.params.id
+            });
+          }
+        })
+        .catch(function (err) {
+          console.log(err.message);
+          res.status(500).json({
+            name: err.name,
+            message: err.message
+          })
+        });
   });
-  app.post('/users', function(req, res){
+
+  app.post('/users', function (req, res) {
     var user = new User(req.body);
     user = user.saveAsync()
-    .then(function(user){
-      res.status(200).json(user);
-    })
-    .catch(function(err){
-      res.status(500).json({ name: err.name, message: err.message });
-    });
+        .then(function (user) {
+          res.status(200).json(user);
+        })
+        .catch(function (err) {
+          res.status(500).json({
+            name: err.name,
+            message: err.message
+          });
+        });
 
   });
 
-  function authenticate(req, res, next){
+  app.post('/users/generate-random/:count', function (req, res) {
+    let count = req.params.count;
+    let user;
+    for(let i=0; i< count; i++) {
+      let json = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        phone: faker.phone.phoneNumber(),
+        dateOfBirth: faker.date.past(),
+        address: {
+          street: faker.address.streetAddress(),
+          city: faker.address.city(),
+          zip: faker.address.zipCode(),
+          state: faker.address.stateAbbr()
+        },
+        company: {
+          name: faker.company.companyName(),
+          address: {
+            street: faker.address.streetAddress(),
+            city: faker.address.city(),
+            zip: faker.address.zipCode(),
+            state: faker.address.stateAbbr()
+          },
+          phone: faker.phone.phoneNumber()
+        },
+        avatar: faker.image.avatar()
+      };
+
+      user = new User(json);
+
+      user.saveAsync()
+          .then(function (user) {
+            console.log('successfully saved user')
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+    }
+
+
+
+  });
+
+  function authenticate(req, res, next) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
     // decode token
     if (token) {
 
       // verifies secret and checks exp
-      jwt.verify(token, app.get('secret'), function(err, decoded) {
+      jwt.verify(token, app.get('secret'), function (err, decoded) {
         if (err) {
-          return res.json({ success: false, message: 'Failed to authenticate token.' });
+          return res.status(403).json({
+            success: false,
+            message: 'Failed to authenticate token.'
+          });
         } else {
           // if everything is good, save to request for use in other routes
           req.decoded = decoded;
@@ -94,17 +170,10 @@ module.exports = function(app){
       // if there is no token
       // return an error
       return res.status(403).send({
-          name: "AuthenticationError",
-          message: 'No token provided.'
+        name: "AuthenticationError",
+        message: 'No token provided.'
       });
 
     }
   }
 };
-
-
-
-/*
-eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NWU3ZDY2N2MzODU2N2NhMjkwZmYzMDMiLCJjcmVhdGVkX2F0IjoiMjAxNS0wOS0wM1QwNToxMTowMy4xNzFaIiwidXBkYXRlZF9hdCI6IjIwMTUtMDktMDNUMDU6MTE6MDMuMTcxWiIsImVtYWlsIjoib3NtYW5AbWFpbC5jb20iLCJwYXNzd29yZCI6IjhmYWQxNDQ3NTczNDBmZDZiMDJiOThmMmJjYTQ5NjNmNDM5YTA3NzcwMTYwZGY3M2ZiZDc4ZjkyOGY2MWM2MDEiLCJmaXJzdE5hbWUiOiJPc21hbiIsImxhc3ROYW1lIjoiT3pkZW1pciIsIl9fdiI6MCwiYWNjb3VudFR5cGUiOiJub3JtYWwifQ.elCa0oub7yF2oE8SBwOZ8ENAMzauXaUXpwy1nEurFVQ
-eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NWU3ZGZhYzYyMDg2NzA2MmU4MmUzYzQiLCJjcmVhdGVkX2F0IjoiMjAxNS0wOS0wM1QwNTo1MDozNi44MjFaIiwidXBkYXRlZF9hdCI6IjIwMTUtMDktMDNUMDU6NTA6MzYuODIxWiIsImVtYWlsIjoibnVtYW5AaG90bWFpbC5jb20iLCJwYXNzd29yZCI6ImRjZWI3ZmM3OTk1MjZkODE2NDVlNTcyOWIyNDBjOTdiYWZlN2ZkOTA1NzQ4M2JlYmE1M2ZjZTRiM2UzZDk5ZWEiLCJmaXJzdE5hbWUiOiJOdW1hbiIsImxhc3ROYW1lIjoiT3pkZW1pciIsIl9fdiI6MCwiYWNjb3VudFR5cGUiOiJub3JtYWwifQ.eS-LZNxnu-d9Hf1OHh3xUjKb1Cf_VwO1VXy61yEEJFM
-*/
